f9806166793c354d87d42e035f5f67f1da6ca92f
Meng-Yuan Huang <myh@live.com>
2021-09-01 09:56:17 +0800

    2.2.0

--- static/js/53.b4604a04.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptfwc=this.webpackJsonptfwc||[]).push([[53],{474:function(t,n,e){"use strict";e.r(n),e.d(n,"startStatusTap",(function(){return o}));var c=e(23),i=e(21),o=function(){var t=window;t.addEventListener("statusTap",(function(){Object(c.h)((function(){var n=t.innerWidth,e=t.innerHeight,o=document.elementFromPoint(n/2,e/2);if(o){var r=o.closest("ion-content");r&&new Promise((function(t){return Object(i.c)(r,t)})).then((function(){Object(c.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.b4604a04.chunk.js.map
